d5ee492f11c09f519c44510d37c93bc91c6c7aac
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-05-28 12:24:32 +0200

    make --quiet options work

--- fastsubtrees/_scripts_support.py
+++ fastsubtrees/_scripts_support.py
@@ -5,6 +5,7 @@ import fastsubtrees
 def setup_verbosity(args):
   if args["--quiet"]:
     fastsubtrees.PROGRESS_ENABLED = False
+    fastsubtrees.logger.remove()
   else:
     fastsubtrees.PROGRESS_ENABLED = True
     if args["--debug"]:
