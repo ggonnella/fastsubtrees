a75b412f1cba6742b5b15fb0dce2e742380b30b7
aman.modi <aman.modi@stud.uni-goettingen.de>
2022-05-30 23:08:51 +0200

    modified test cases for deleting the subtree

--- tests/test_fastsubtrees.py
+++ tests/test_fastsubtrees.py
@@ -58,17 +58,29 @@ class TestClass:
 
     def test_fastsubtrees_query_smalltree(self):
         # use subtree_ids 1, 8 and change the id in variable results_query_smalltree_id_*
+        generator = element_parent_ids('../tests/testdata/small_tree.tsv')
+        tree = Tree.construct(generator)
+        outfname = Path('../tests/small_tree.tree')
+        tree.to_file(outfname)
         tree = Tree.from_file('../tests/small_tree.tree')
         subtree_ids = tree.subtree_ids(1)
         assert subtree_ids == results_query_smalltree_id_1
 
     def test_fastsubtrees_query_middletree(self):
         # use subtree_ids 1, 8, 566 and change the id in variable results_query_middletree_id_*
+        generator = element_parent_ids('../tests/testdata/middle_tree.tsv')
+        tree = Tree.construct(generator)
+        outfname = Path('../tests/middle_tree.tree')
+        tree.to_file(outfname)
         tree = Tree.from_file('../tests/middle_tree.tree')
         subtree_ids = tree.subtree_ids(1)
         assert subtree_ids == results_query_middletree_id_1
 
     def test_query_node_not_exist(self):
+        generator = element_parent_ids('../tests/testdata/small_tree.tsv')
+        tree = Tree.construct(generator)
+        outfname = Path('../tests/small_tree.tree')
+        tree.to_file(outfname)
         tree = Tree.from_file('../tests/small_tree.tree')
         with pytest.raises(error.NodeNotFoundError):
             tree.subtree_ids(87)
@@ -141,39 +153,37 @@ class TestClass:
         with pytest.raises(error.DeletedNodeError):
             new_tree.add_subtree(generator)
             os.remove('../tests/add_already_deleted_node.tree')
-    #
-    #
-    # def test_fastsubtrees_delete_node_smalltree(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/small_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/small_tree').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/small_tree.tree')
-    #     tree.delete_node(3)
-    #     tree.to_file('./tests/small_tree.tree')
-    #     new_tree = Tree.from_file('./tests/small_tree.tree')
-    #     subtree_ids = new_tree.subtree_ids(1)
-    #     os.remove('./tests/small_tree.tree')
-    #     assert subtree_ids == results_delete_subtree_smalltree_id_1
-    #
-    #
-    # def test_fastsubtrees_delete_node_middletree(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/middle_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/middle_tree').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/middle_tree.tree')
-    #     tree.delete_node(78)
-    #     tree.to_file('./tests/middle_tree.tree')
-    #     new_tree = Tree.from_file('./tests/middle_tree.tree')
-    #     subtree_ids = new_tree.subtree_ids(46)
-    #     os.remove('./tests/middle_tree.tree')
-    #     assert subtree_ids == results_delete_subtree_middletree_id_78
-    #
-    # def test_delete_node_not_exist(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/small_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/small_tree').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/small_tree.tree')
-    #     with pytest.raises(error.NodeNotFoundError):
-    #         tree.delete_node(99)
-    #         os.remove('./tests/small_tree.tree')
+
+    def test_fastsubtrees_delete_node_smalltree(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/small_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/small_tree').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/small_tree.tree')
+        tree.delete_node(3)
+        tree.to_file('../tests/small_tree.tree')
+        new_tree = Tree.from_file('../tests/small_tree.tree')
+        subtree_ids = new_tree.subtree_ids(1)
+        os.remove('../tests/small_tree.tree')
+        assert subtree_ids == results_delete_subtree_smalltree_id_1
+
+    def test_fastsubtrees_delete_node_middletree(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/middle_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/middle_tree').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/middle_tree.tree')
+        tree.delete_node(78)
+        tree.to_file('../tests/middle_tree.tree')
+        new_tree = Tree.from_file('../tests/middle_tree.tree')
+        subtree_ids = new_tree.subtree_ids(46)
+        os.remove('../tests/middle_tree.tree')
+        assert subtree_ids == results_delete_subtree_middletree_id_78
+
+    def test_delete_node_not_exist(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/small_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/small_tree').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/small_tree.tree')
+        with pytest.raises(error.NodeNotFoundError):
+            tree.delete_node(99)
+            os.remove('../tests/small_tree.tree')
 
