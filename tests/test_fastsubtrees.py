239735582c07aeee8aa1b6bb39c872d6f97ec769
aman.modi <aman.modi@stud.uni-goettingen.de>
2022-05-30 17:28:25 +0200

    tests updated for adding subtrees

--- tests/test_fastsubtrees.py
+++ tests/test_fastsubtrees.py
@@ -72,81 +72,75 @@ class TestClass:
         tree = Tree.from_file('../tests/small_tree.tree')
         with pytest.raises(error.NodeNotFoundError):
             tree.subtree_ids(87)
-    #
-    # def test_fastsubtrees_add_subtree_smalltree(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/small_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/small_tree').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/small_tree.tree')
-    #     elemparentid = element_parent_ids('./tests/testdata/smalltree_add_subtree.tsv')
-    #     generator = elemparentid.get_element_parent_id()
-    #     tree.add_subtree(generator)
-    #     tree.to_file('./tests/small_tree.tree')
-    #     new_tree = Tree.from_file('./tests/small_tree.tree')
-    #     subtree_ids = new_tree.subtree_ids(1)
-    #     os.remove('./tests/small_tree.tree')
-    #     assert subtree_ids == results_add_subtree_smalltree_id_1
-    #
-    #
-    # def test_fastsubtrees_add_subtree_middletree(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/middle_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/middle_tree').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/middle_tree.tree')
-    #     elemparentid = element_parent_ids('./tests/testdata/middletree_add_subtree.tsv')
-    #     generator = elemparentid.get_element_parent_id()
-    #     tree.add_subtree(generator)
-    #     tree.to_file('./tests/middle_tree.tree')
-    #     new_tree = Tree.from_file('./tests/middle_tree.tree')
-    #     subtree_ids = new_tree.subtree_ids(46)
-    #     os.remove('./tests/middle_tree.tree')
-    #     assert subtree_ids == results_add_subtree_middletree_id_46
-    #
-    # def test_add_subtree_repeated_node_ids(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/small_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/small_tree_repeated_node_id').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/small_tree_repeated_node_id.tree')
-    #     elemparentid = element_parent_ids('./tests/testdata/add_subtree_repeated_nodes.tsv')
-    #     generator = elemparentid.get_element_parent_id()
-    #     with pytest.raises(error.ConstructionError):
-    #         tree.add_subtree(generator)
-    #         os.remove('./tests/small_tree_repeated_node_id.tree')
-    #
-    # def test_add_subtree_parent_not_exist(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/small_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/small_tree_repeated_node_id').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/small_tree_repeated_node_id.tree')
-    #     elemparentid = element_parent_ids('./tests/testdata/add_subtree_parent_not_exist.tsv')
-    #     generator = elemparentid.get_element_parent_id()
-    #     with pytest.raises(error.ConstructionError):
-    #         tree.add_subtree(generator)
-    #         os.remove('./tests/small_tree_repeated_node_id.tree')
-    #
-    # def test_add_subtree_node0(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/small_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/small_tree_repeated_node_id').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/small_tree_repeated_node_id.tree')
-    #     elemparentid = element_parent_ids('./tests/testdata/construction_node0.tsv')
-    #     generator = elemparentid.get_element_parent_id()
-    #     with pytest.raises(error.ConstructionError):
-    #         tree.add_subtree(generator)
-    #         os.remove('./tests/small_tree_repeated_node_id.tree')
-    #
-    # def test_add_already_deleted_node(self):
-    #     create_tree = Tree.construct_from_csv('./tests/testdata/small_tree.tsv', '\t', 0, 1)
-    #     outfname = Path('./tests/add_already_deleted_node').with_suffix(".tree")
-    #     create_tree.to_file(outfname)
-    #     tree = Tree.from_file('./tests/git.tree')
-    #     tree.delete_node(3)
-    #     new_tree = Tree.from_file('./tests/add_already_deleted_node.tree')
-    #     elemparentid = element_parent_ids('./tests/testdata/add_already_deleted_node.tsv')
-    #     generator = elemparentid.get_element_parent_id()
-    #     with pytest.raises(error.DeletedNodeError):
-    #         new_tree.add_subtree(generator)
-    #         os.remove('./tests/add_already_deleted_node.tree')
+
+    def test_fastsubtrees_add_subtree_smalltree(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/small_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/small_tree').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/small_tree.tree')
+        generator = element_parent_ids('../tests/testdata/smalltree_add_subtree.tsv')
+        tree.add_subtree(generator)
+        tree.to_file('../tests/small_tree.tree')
+        new_tree = Tree.from_file('../tests/small_tree.tree')
+        subtree_ids = new_tree.subtree_ids(1)
+        os.remove('../tests/small_tree.tree')
+        assert subtree_ids == results_add_subtree_smalltree_id_1
+
+    def test_fastsubtrees_add_subtree_middletree(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/middle_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/middle_tree').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/middle_tree.tree')
+        generator = element_parent_ids('../tests/testdata/middletree_add_subtree.tsv')
+        tree.add_subtree(generator)
+        tree.to_file('../tests/middle_tree.tree')
+        new_tree = Tree.from_file('../tests/middle_tree.tree')
+        subtree_ids = new_tree.subtree_ids(46)
+        os.remove('../tests/middle_tree.tree')
+        assert subtree_ids == results_add_subtree_middletree_id_46
+
+    def test_add_subtree_repeated_node_ids(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/small_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/small_tree_repeated_node_id').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/small_tree_repeated_node_id.tree')
+        generator = element_parent_ids('../tests/testdata/add_subtree_repeated_nodes.tsv')
+        with pytest.raises(error.ConstructionError):
+            tree.add_subtree(generator)
+            os.remove('../tests/small_tree_repeated_node_id.tree')
+
+    def test_add_subtree_parent_not_exist(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/small_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/small_tree_parent_not_exist').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/small_tree_parent_not_exist.tree')
+        generator = element_parent_ids('../tests/testdata/add_subtree_parent_not_exist.tsv')
+        with pytest.raises(error.ConstructionError):
+            tree.add_subtree(generator)
+            os.remove('../tests/small_tree_parent_not_exist.tree')
+
+    def test_add_subtree_node0(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/small_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/small_tree_add_node0').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/small_tree_add_node0.tree')
+        generator = element_parent_ids('../tests/testdata/construction_node0.tsv')
+        with pytest.raises(error.ConstructionError):
+            tree.add_subtree(generator)
+            os.remove('../tests/small_tree_add_node0.tree')
+
+    def test_add_already_deleted_node(self):
+        create_tree = Tree.construct_from_csv('../tests/testdata/small_tree.tsv', '\t', 0, 1)
+        outfname = Path('../tests/add_already_deleted_node').with_suffix(".tree")
+        create_tree.to_file(outfname)
+        tree = Tree.from_file('../tests/add_already_deleted_node.tree')
+        tree.delete_node(3)
+        tree.to_file(outfname)
+        new_tree = Tree.from_file('../tests/add_already_deleted_node.tree')
+        generator = element_parent_ids('../tests/testdata/add_already_deleted_node.tsv')
+        with pytest.raises(error.DeletedNodeError):
+            new_tree.add_subtree(generator)
+            os.remove('../tests/add_already_deleted_node.tree')
     #
     #
     # def test_fastsubtrees_delete_node_smalltree(self):
