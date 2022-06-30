ed696fbf5a3195fd80220761b741fb6924316dd6
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-06-30 11:51:15 +0200

    use rel paths in generate_attr file script in example

--- examples/generate_attribute_file.py
+++ examples/generate_attribute_file.py
@@ -1,6 +1,7 @@
 import json
 from collections import defaultdict
 import gzip
+import os
 
 import fastsubtrees
 
@@ -9,8 +10,10 @@ GENOME_SIZE_COLUMN = 2
 GC_CONTENT_COLUMN = 3
 attributes = ['genome_size', 'GC_content']
 
+scriptdir = os.path.dirname(os.path.realpath(__file__))
+
 def generate_attribute_file():
-  with gzip.open('./accession_taxid_attribute.tsv.gz', 'r') as file:
+  with gzip.open(f'{scriptdir}/accession_taxid_attribute.tsv.gz', 'r') as file:
     taxid_genome_dictionary = defaultdict(list)
     taxid_gccontent_dictionary = defaultdict(list)
     for line in file:
@@ -18,8 +21,8 @@ def generate_attribute_file():
       taxid_genome_dictionary[int(elems[TAXID_COLUMN])].append(int(elems[GENOME_SIZE_COLUMN]))
       taxid_gccontent_dictionary[int(elems[TAXID_COLUMN])].append(float(elems[GC_CONTENT_COLUMN]))
   for attribute in attributes:
-    with open(f'{attribute}.attr', 'w') as outfile:
-      tree = fastsubtrees.Tree.from_file('./nt.tree')
+    with open(f'{scriptdir}/{attribute}.attr', 'w') as outfile:
+      tree = fastsubtrees.Tree.from_file(f'{scriptdir}/nt.tree')
       for element_id in tree.subtree_ids(1):
         attribute = taxid_genome_dictionary.get(element_id, None)
         outfile.write(json.dumps(attribute) + "\n")
