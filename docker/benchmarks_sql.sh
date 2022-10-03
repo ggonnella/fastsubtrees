1fbb0c2529758b24390152ee6d7e2b1eb1236d87
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-10-03 15:13:50 +0200

    more work on benchmarks in docker

--- docker/benchmarks_sql.sh
+++ docker/benchmarks_sql.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 NREPEATS=3
+OUTFILE=benchmarks_sql.tsv
 
+rm -f $OUTFILE
 for ((i=0; i<$NREPEATS; i++)); do
-    echo "Iteration $i"
-    /usr/bin/time ntmirror-dbload --testmode --reset \
-      myuser mypass ntmirror_test /run/mysqld/mysqld.sock ntdumpdir
+    STEP="dbload"
+    VAR=""
+    echo "Step $STEP, iteration $i..."
+    /usr/bin/time -f "$STEP\t$VAR\t$i\t%U\t%S\t%e\t%M" -o $OUTFILE -a \
+      ntmirror-dbload --testmode --reset \
+        myuser mypass ntmirror_test /run/mysqld/mysqld.sock ntdumpdir
 done
 
