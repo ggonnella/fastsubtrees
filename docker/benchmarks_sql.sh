3e9b00665d5168712eefbc157b9addde34a08210
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2022-10-03 14:55:45 +0200

    more work on benchmarks under docker

--- docker/benchmarks_sql.sh
+++ docker/benchmarks_sql.sh
@@ -1,2 +1,10 @@
-time ntmirror-dbload --reset myuser mypass ntmirror_test /run/mysqld/mysqld.sock ntdumpdir
+#!/bin/bash
+
+NREPEATS=3
+
+for ((i=0; i<$NREPEATS; i++)); do
+    echo "Iteration $i"
+    /usr/bin/time ntmirror-dbload --testmode --reset \
+      myuser mypass ntmirror_test /run/mysqld/mysqld.sock ntdumpdir
+done
 
